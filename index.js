c4a7fd5332918cf870d7a4c1b3e182c8e62c8267
Nolan Van Hell <nolanvh@uvic.ca>
2020-10-03 00:29:21 -0700

    Fix package.json start script and update port in index.js

--- index.js
+++ index.js
@@ -55,5 +55,5 @@ io.on('connection', socket => {
 const port = process.env.port || 3000;
 
 server.listen(port, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${port}`)
 })
